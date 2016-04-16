cac60acf73dbdef2c490abbef975935055b6519d
Shabad Lamba <shabadlamba@gmail.com>
2016-04-16 22:03:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+from flask import Flask, request, g, redirect, url_for, render_template
+import json
+from re import sub
+
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+#url for medicine suggestions
+url = "http://www.truemd.in/api/medicine_suggestions/"
+
+#url for alternatives of a mdicine
+# url = "http://www.truemd.in/api/medicine_alternatives/"
+
+#api key
+apiKey = "d3687c2bd51423322acf02a2f6f17b"
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/',methods=['GET','POST'])
+def MedinceSuggestions():
+	if request.method == 'POST':
+	#takign input from the user
+		searchString = request.form['text']
+	#search parameters
+		Parameters = {
+			"id": searchString,
+			"key": apiKey,
+			"limit":"10"
+		}
+		medicineList = get_Data(Parameters)
+		print medicineList
+		return redirect(url_for('show_entries', medicineList=medicineList))
+	
+	return render_template('medicore.html')#, medicineList = medicineList)
+# for medicines in Data["response"]["medicine_alternatives"]:
+# 	print medicines.key() + " : " + medicines.value()
+
+def get_Data(Parameters):
+	medicineData = []
+	Response = requests.get(url, params=Parameters).content
+	Data = json.loads(Response)
+	print Data
+	for suggestion in Data["response"]["suggestions"]:
+		medicineData.append(sub(r"[^\x00-\x7F]+","",suggestion["suggestion"]))
+	print "1"
+	print medicineData
+	return medicineData
+
+@app.route('/result/<medicineList>')
+def show_entries(medicineList):
+	print type(medicineList)
+	medicineList = "".join(medicineList)
+	print "".join("".join(medicineList))
+	return render_template('result.html',medicineList=medicineList)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5008)
